1195a440060ef336ab9fa2e7bab49969a1185a14
Anwita <anwita.tiwari@gmail.com>
2025-07-28 10:32:27 -0700

    Added things

---
+++ ToDoList/NewToDo.swift
@@ -0,0 +1,39 @@
+//
+//  NewToDo.swift
+//  ToDoList
+//
+//  Created by Anwita Tiwari on 7/28/25.
+//
+
+import SwiftUI
+
+struct NewToDo: View {
+    var body: some View {
+        VStack{
+            Text("Task Title")
+                .font(.title)
+                .fontWeight(.bold)
+            TextField("Enter yor task description", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(20)
+                .padding()
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+                    
+                    
+            }
+            Button{
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            } label: {
+            Text("Save")
+            }
+        }
+        
+        .padding()
+    }
+}
+
+#Preview {
+    NewToDo()
+}
