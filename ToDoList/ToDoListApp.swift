79f649af5fc5685aaebe7cf8e70550d3ef3b9a44
Anwita <anwita.tiwari@gmail.com>
2025-07-28 09:57:25 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Anwita Tiwari on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
